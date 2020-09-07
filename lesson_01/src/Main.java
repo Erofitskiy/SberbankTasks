c416c64acdd1d9654b03d0dac240b66d44c08262
MRXover <14204072+MRXover@users.noreply.github.com>
2020-09-07 18:12:25 +0300

    Update Main.java

--- lesson_01/src/Main.java
+++ lesson_01/src/Main.java
@@ -36,12 +36,12 @@ public class Main {
     }
 
     public static void bubbleSort(List<Integer> list){
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (list.get(i) < list.get(j)){
-                    Integer s = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, s);
+        for (int i = 0; i < list.size()- 1; i++) {
+            for (int j = 0; j < list.size() - 1 - i; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    int temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
                 }
             }
         }
